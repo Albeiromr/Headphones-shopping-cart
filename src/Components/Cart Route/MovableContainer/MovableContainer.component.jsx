2d5e2c33ddc1ef4136fa97c7d5729c34f802ccb6
Albeiro Marin <xlandru@hotmail.com>
2020-10-13 17:17:05 -0500

    The state for moving the movable container is readey

--- src/Components/Cart Route/MovableContainer/MovableContainer.component.jsx
+++ src/Components/Cart Route/MovableContainer/MovableContainer.component.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Proptypes from "prop-types";
 import './1000-1920px.style.scss';
 import './500-1000px.style.scss';
 import './0-500px.style.scss';
 
 const MovableContainer = () => {
-
+  
+  // this state is for moving this container side to side
+  const [translate, changeTranslate] = useState('0vw');
 
     return (
+        <div 
+        className='movable-container'
+        style={{'--translate': translate}}
+        >
+
+        </div>
         
     )
 };
--- src/Components/Cart Route/ShoppingCart/ShoppingCart.compounent.jsx
+++ src/Components/Cart Route/ShoppingCart/ShoppingCart.compounent.jsx
@@ -3,13 +3,16 @@ import Proptypes from "prop-types";
 import './1000-1920px.style.scss';
 import './500-1000px.style.scss';
 import './0-500px.style.scss';
+import MovableContainer from '../MovableContainer/MovableContainer.component';
 
 const ShoppingCart = () => {
 
+    
 
     return (
         <section className='shopping-cart'>
-            <h1 className='a'> este es el carrito</h1>
+            < MovableContainer
+            />
         </section>
     )
 };
