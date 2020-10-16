5817a5b2562bb25e4aca666c7a927c89cbb13aa6
Albeiro Marin <xlandru@hotmail.com>
2020-10-16 16:46:24 -0500

    products inplemented in shopping cart for big screens

--- src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
+++ src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
@@ -6,11 +6,17 @@ import './0-500px.style.scss';
 
 const CartProduct = ({cartProduct}) => {
 
-    let {color, image, name, price} = cartProduct;
+    let {color, image, name, price, id} = cartProduct;
 
     return (
         <div className='cart-product'>
-            
+
+            <article className='cart-product__sub-container'>
+
+            </article>
+
+            <img className='cart-product__image' src={image} alt="product image"/>
+
         </div>
     )
 };
