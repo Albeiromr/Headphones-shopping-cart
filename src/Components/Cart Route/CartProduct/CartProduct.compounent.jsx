7534e15ac5cc6eced1a4a37de58128ca65de12b0
Albeiro Marin <xlandru@hotmail.com>
2020-10-16 20:53:34 -0500

    Just the delete button left

--- src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
+++ src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
@@ -3,24 +3,41 @@ import Proptypes from "prop-types";
 import './1000-1920px.style.scss';
 import './500-1000px.style.scss';
 import './0-500px.style.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
 
 const CartProduct = ({cartProduct}) => {
 
     let {color, image, name, price, id} = cartProduct;
 
     return (
-        <div className='cart-product'>
+      <div className="cart-product">
+        <article className="cart-product__sub-container">
+          <h2 className="cart-product__reference">{name}</h2>
+          <p className="cart-product__color">{color}</p>
+          <p className="cart-product__price">$ {price}</p>
 
-            <article className='cart-product__sub-container'>
-                <h2 className='cart-product__reference'>{name}</h2>
-                <p className='cart-product__color'>{color}</p>
-                <p className='cart-product__price'>$ {price}</p>
-            </article>
+          <div className='cart-product__quantity-container'>
 
-            <img className='cart-product__image' src={image} alt="product image"/>
+                <FontAwesomeIcon 
+                className='cart-product__add-less'
+                icon={faMinusCircle}
+                />
 
-        </div>
-    )
+                <p className='cart-product__quantity'>1</p>
+
+                <FontAwesomeIcon
+                className='cart-product__add-less'
+                icon={faPlusCircle}
+                />
+
+          </div>
+
+        </article>
+
+        <img className="cart-product__image" src={image} alt="product image" />
+      </div>
+    );
 };
 
 CartProduct.propTypes = {
