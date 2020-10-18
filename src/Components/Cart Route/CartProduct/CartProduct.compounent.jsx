9a1b14f135352c7e2e7e4dc54c8e77a37f01376f
Albeiro Marin <xlandru@hotmail.com>
2020-10-18 09:26:39 -0500

    Delete button added to shopping cart element

--- src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
+++ src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
@@ -4,7 +4,7 @@ import './1000-1920px.style.scss';
 import './500-1000px.style.scss';
 import './0-500px.style.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle, faMinusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const CartProduct = ({cartProduct}) => {
 
@@ -16,6 +16,11 @@ const CartProduct = ({cartProduct}) => {
           <h2 className="cart-product__reference">{name}</h2>
           <p className="cart-product__color">{color}</p>
           <p className="cart-product__price">$ {price}</p>
+
+                <FontAwesomeIcon 
+                className='cart-product__close'
+                icon={faTimesCircle}
+                />
 
           <div className='cart-product__quantity-container'>
 
