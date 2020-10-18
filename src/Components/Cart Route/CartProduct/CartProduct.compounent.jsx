4180db842b957ba1986955fe35cd685ebb13748c
Albeiro Marin <xlandru@hotmail.com>
2020-10-18 10:01:21 -0500

    price fixed individualy for shopping cart elements

--- src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
+++ src/Components/Cart Route/CartProduct/CartProduct.compounent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Proptypes from "prop-types";
 import './1000-1920px.style.scss';
 import './500-1000px.style.scss';
@@ -6,16 +6,42 @@ import './0-500px.style.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle, faMinusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
+
 const CartProduct = ({cartProduct}) => {
 
+
+    const [quantity, changeQuantity] =useState(1)
+    // cartProduct destructuring
     let {color, image, name, price, id} = cartProduct;
+    // counter for incrmenting product quantity
+    let increment = quantity
+    // price * quantity
+    let priceMultiplied = price * quantity
+
+    const handlePlusClick = () => {
+      // this is the action taken when clicking in the plus button
+      increment++
+      changeQuantity(increment)
+    }
+
+    const handleMinusClick = () => {
+      // this is the action taken when clicking in the minus button
+      
+      if(quantity < 2){
+        changeQuantity(1)
+      }else {
+        increment--
+        changeQuantity(increment)
+      }
+      
+    }
 
     return (
       <div className="cart-product">
         <article className="cart-product__sub-container">
           <h2 className="cart-product__reference">{name}</h2>
           <p className="cart-product__color">{color}</p>
-          <p className="cart-product__price">$ {price}</p>
+          <p className="cart-product__price">$ {priceMultiplied.toFixed(2)}</p>
 
                 <FontAwesomeIcon 
                 className='cart-product__close'
@@ -25,13 +51,15 @@ const CartProduct = ({cartProduct}) => {
           <div className='cart-product__quantity-container'>
 
                 <FontAwesomeIcon 
+                onClick={handleMinusClick}
                 className='cart-product__add-less'
                 icon={faMinusCircle}
                 />
 
-                <p className='cart-product__quantity'>1</p>
+                <p className='cart-product__quantity'>{quantity}</p>
 
                 <FontAwesomeIcon
+                onClick={handlePlusClick}
                 className='cart-product__add-less'
                 icon={faPlusCircle}
                 />
