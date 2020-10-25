09c8ca5c1fab70eeb9654b85e266969f8526da84
Albeiro Marin <xlandru@hotmail.com>
2020-10-25 17:05:10 -0500

    spiner coded successfuly

--- src/Components/Cart Route/SniperSquare/SniperSquare.compounent.jsx
+++ src/Components/Cart Route/SniperSquare/SniperSquare.compounent.jsx
@@ -3,13 +3,18 @@ import Proptypes from "prop-types";
 import "./1000-1920px.style.scss";
 import "./500-1000px.style.scss";
 import "./0-500px.style.scss";
+import Sniper from '../Sniper/Sniper.compounent';
 
 const SniperSquare = () => {
 
     return (
 
         <div className='sniper-square'>
-            
+            <Sniper />
+
+            <p className='sniper-square__text'>
+            We are verifying your payment
+            </p>
         </div>
     )
 };
--- src/Components/Cart Route/SniperSquareBig/SniperSquareBig.compounent.jsx
+++ src/Components/Cart Route/SniperSquareBig/SniperSquareBig.compounent.jsx
@@ -3,13 +3,18 @@ import Proptypes from "prop-types";
 import "./1000-1920px.style.scss";
 import "./500-1000px.style.scss";
 import "./0-500px.style.scss";
+import Sniper from '../Sniper/Sniper.compounent';
 
 const SniperSquareBig = () => {
 
     return (
 
         <div className='sniper-square-big'>
+            <Sniper />
 
+            <p className='sniper-square-big__text'>
+            We are verifying your payment
+            </p>
         </div>
     )
 };
