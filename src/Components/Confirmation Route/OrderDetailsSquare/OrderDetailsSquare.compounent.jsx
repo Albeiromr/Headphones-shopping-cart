30b447adcac3056ee4ba6090896e79541c007137
Albeiro Marin <xlandru@hotmail.com>
2020-10-27 11:14:53 -0500

    lines and arrow set in their positions

--- src/Components/Confirmation Route/OrderDetailsSquare/OrderDetailsSquare.compounent.jsx
+++ src/Components/Confirmation Route/OrderDetailsSquare/OrderDetailsSquare.compounent.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import Proptypes from "prop-types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import "./1000-1920px.style.scss";
 import "./500-1000px.style.scss";
 import "./0-500px.style.scss";
 
 const OrderDetailsSquare = () => {
 
+    // this state is for moving the whole order-details-square in smartphones displays
+    const [move, setMove] = useState('0%')
+
+    const handleLeft = () => {
+        setMove('-53.358%')
+    };
+
+    const handleRight = () => {
+        setMove('0%')
+    };
+
     return (
-        <div className='order-details-square'>
+        <div style={{'--move': move}} className='order-details-square'>
+
+            <div className='order-details-square__line1'></div>
+
+            <div className='order-details-square__line2'></div>
+
+            <FontAwesomeIcon
+            onClick={handleLeft}
+            icon={faChevronLeft}
+            className="order-details-square__arrow-left"
+            />
+
+            <FontAwesomeIcon
+            onClick={handleRight}
+            icon={faChevronRight}
+            className="order-details-square__arrow-right"
+            />
+            
 
         </div>
     )
