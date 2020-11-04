efd242464a2b9c1bc76db5d76aa73b299b35c82e
Albeiro Marin <xlandru@hotmail.com>
2020-11-04 10:09:02 -0500

    water mark added

--- src/Components/Store Route/GrayColumn/GrayColumn.component.jsx
+++ src/Components/Store Route/GrayColumn/GrayColumn.component.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import './1000-1920px.style.scss';
-import './500-1000px.style.scss';
-import './0-500px.style.scss';
+import React from "react";
+import "./1000-1920px.style.scss";
+import "./500-1000px.style.scss";
+import "./0-500px.style.scss";
 
-const GrayColumn = () => (<div className='Gray-Column'></div>)
+const GrayColumn = () => (
+  <div className="Gray-Column">
+    <p className="Gray-Column__water-mark">Albeiro Marín GitHub @Albeiromr</p>
+  </div>
+);
 
 export default GrayColumn;
