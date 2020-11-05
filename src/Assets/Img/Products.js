697913df7fdc4ab3f1b0e0abeb38876f55bd304f
Albeiro Marin <xlandru@hotmail.com>
2020-11-05 18:51:38 -0500

    small size images charge on local folder and imported in the products java script file

--- src/Assets/Img/Products.js
+++ src/Assets/Img/Products.js
@@ -7,12 +7,30 @@ import n90Second from './akg n90/n90Second.png';
 import n90SecondBlue from './akg n90/n90SecondBlue.png';
 import n90SecondGreen from './akg n90/n90SecondGreen.png';
 
+import thumbnailn90Main from './akg n90/thumbnailn90Main.png';
+import thumbnailn90MainBlue from './akg n90/thumbnailn90MainBlue.png';
+import thumbnailn90MainGreen from './akg n90/thumbnailn90MainGreen.png';
+import thumbnailn90Second from './akg n90/thumbnailn90Second.png';
+import thumbnailn90SecondBlue from './akg n90/thumbnailn90SecondBlue.png';
+import thumbnailn90SecondGreen from './akg n90/thumbnailn90SecondGreen.png';
+
+////////////////////////////////////////////////////////////////////////////
+
 import n700ncMain from './akg n700nc/n700ncMain.png';
 import n700ncMainBrown from './akg n700nc/n700ncMainBrown.png';
 import n700ncMainGreen from './akg n700nc/n700ncMainGreen.png';
 import n700ncSecond from './akg n700nc/n700ncSecond.png';
 import n700ncSecondBrown from './akg n700nc/n700ncSecondBrown.png';
 import n700ncSecondGreen from './akg n700nc/n700ncSecondGreen.png';
+
+import thumbnailn700ncMain from './akg n700nc/thumbnailn700ncMain.png';
+import thumbnailn700ncMainBrown from './akg n700nc/thumbnailn700ncMainBrown.png';
+import thumbnailn700ncMainGreen from './akg n700nc/thumbnailn700ncMainGreen.png';
+import thumbnailn700ncSecond from './akg n700nc/thumbnailn700ncSecond.png';
+import thumbnailn700ncSecondBrown from './akg n700nc/thumbnailn700ncSecondBrown.png';
+import thumbnailn700ncSecondGreen from './akg n700nc/thumbnailn700ncSecondGreen.png';
+
+////////////////////////////////////////////////////////////////////////////
 
 import y40Main from './akg y40/y40Main.png';
 import y40MainBrown from './akg y40/y40MainBrown.png';
@@ -21,12 +39,30 @@ import y40Second from './akg y40/y40Second.png';
 import y40SecondBrown from './akg y40/y40SecondBrown.png';
 import y40SecondPink from './akg y40/y40SecondPink.png';
 
+import thumbnaily40Main from './akg y40/thumbnaily40Main.png';
+import thumbnaily40MainBrown from './akg y40/thumbnaily40MainBrown.png';
+import thumbnaily40MainPink from './akg y40/thumbnaily40MainPink.png';
+import thumbnaily40Second from './akg y40/thumbnaily40Second.png';
+import thumbnaily40SecondBrown from './akg y40/thumbnaily40SecondBrown.png';
+import thumbnaily40SecondPink from './akg y40/thumbnaily40SecondPink.png';
+
+/////////////////////////////////////////////////////////////////////////////
+
 import liberatexlMain from './house of marle liberate xl/liberatexlMain.png';
 import liberatexlMainGreen from './house of marle liberate xl/liberatexlMainGreen.png';
 import liberatexlMainPurple from './house of marle liberate xl/liberatexlMainPurple.png';
 import liberatexlSecond from './house of marle liberate xl/liberatexlSecond.png';
 import liberatexlSecondGreen from './house of marle liberate xl/liberatexlSecondGreen.png';
 import liberatexlSecondPurple from './house of marle liberate xl/liberatexlSecondPurple.png';
+
+import thumbnailliberatexlMain from './house of marle liberate xl/thumbnailliberatexlMain.png';
+import thumbnailliberatexlMainGreen from './house of marle liberate xl/thumbnailliberatexlMainGreen.png';
+import thumbnailliberatexlMainPurple from './house of marle liberate xl/thumbnailliberatexlMainPurple.png';
+import thumbnailliberatexlSecond from './house of marle liberate xl/thumbnailliberatexlSecond.png';
+import thumbnailliberatexlSecondGreen from './house of marle liberate xl/thumbnailliberatexlSecondGreen.png';
+import thumbnailliberatexlSecondPurple from './house of marle liberate xl/thumbnailliberatexlSecondPurple.png';
+
+///////////////////////////////////////////////////////////////////////////////
 
 import duetMain from './jbl duet/duetMain.png';
 import duetMainGreen from './jbl duet/duetMainGreen.png';
@@ -35,12 +71,30 @@ import duetSecond from './jbl duet/duetSecond.png';
 import duetSecondGreen from './jbl duet/duetSecondGreen.png';
 import duetSecondpink from './jbl duet/duetSecondPink.png';
 
+import thumbnailduetMain from './jbl duet/thumbnailduetMain.png';
+import thumbnailduetMainGreen from './jbl duet/thumbnailduetMainGreen.png';
+import thumbnailduetMainPink from './jbl duet/thumbnailduetMainPink.png';
+import thumbnailduetSecond from './jbl duet/thumbnailduetSecond.png';
+import thumbnailduetSecondGreen from './jbl duet/thumbnailduetSecondGreen.png';
+import thumbnailduetSecondpink from './jbl duet/thumbnailduetSecondPink.png';
+
+////////////////////////////////////////////////////////////////////////////////
+
 import e40btMain from './jbl e40bt/e40btMain.png';
 import e40btMainGreen from './jbl e40bt/e40btMainGreen.png';
 import e40btMainRed from './jbl e40bt/e40btMainRed.png';
 import e40btSecond from './jbl e40bt/e40btSecond.png';
 import e40btSecondGreen from './jbl e40bt/e40btSecondGreen.png';
 import e40btSecondRed from './jbl e40bt/e40btSecondRed.png';
+
+import thumbnaile40btMain from './jbl e40bt/thumbnaile40btMain.png';
+import thumbnaile40btMainGreen from './jbl e40bt/thumbnaile40btMainGreen.png';
+import thumbnaile40btMainRed from './jbl e40bt/thumbnaile40btMainRed.png';
+import thumbnaile40btSecond from './jbl e40bt/thumbnaile40btSecond.png';
+import thumbnaile40btSecondGreen from './jbl e40bt/thumbnaile40btSecondGreen.png';
+import thumbnaile40btSecondRed from './jbl e40bt/thumbnaile40btSecondRed.png';
+
+//////////////////////////////////////////////////////////////////////////////
 
 import endurancediveMain from './jbl endurance dive/endurancediveMain.png';
 import endurancediveMainPink from './jbl endurance dive/endurancediveMainPink.png';
@@ -49,6 +103,15 @@ import endurancediveSecond from './jbl endurance dive/endurancediveSecond.png';
 import endurancediveSecondPink from './jbl endurance dive/endurancediveSecondPink.png';
 import endurancediveSecondRed from './jbl endurance dive/endurancediveSecondRed.png';
 
+import thumbnailendurancediveMain from './jbl endurance dive/thumbnailendurancediveMain.png';
+import thumbnailendurancediveMainPink from './jbl endurance dive/thumbnailendurancediveMainPink.png';
+import thumbnailendurancediveMainRed from './jbl endurance dive/thumbnailendurancediveMainRed.png';
+import thumbnailendurancediveSecond from './jbl endurance dive/thumbnailendurancediveSecond.png';
+import thumbnailendurancediveSecondPink from './jbl endurance dive/thumbnailendurancediveSecondPink.png';
+import thumbnailendurancediveSecondRed from './jbl endurance dive/thumbnailendurancediveSecondRed.png';
+
+///////////////////////////////////////////////////////////////////////////////////
+
 import endurancepeakMain from './jbl endurance peak/endurancepeakMain.png';
 import endurancepeakMainBlue from './jbl endurance peak/endurancepeakMainBlue.png';
 import endurancepeakMainGreen from './jbl endurance peak/endurancepeakMainGreen.png';
@@ -56,12 +119,30 @@ import endurancepeakSecond from './jbl endurance peak/endurancepeakSecond.png';
 import endurancepeakSecondBlue from './jbl endurance peak/endurancepeakSecondBlue.png';
 import endurancepeakSecondGreen from './jbl endurance peak/endurancepeakSecondGreen.png';
 
+import thumbnailendurancepeakMain from './jbl endurance peak/thumbnailendurancepeakMain.png';
+import thumbnailendurancepeakMainBlue from './jbl endurance peak/thumbnailendurancepeakMainBlue.png';
+import thumbnailendurancepeakMainGreen from './jbl endurance peak/thumbnailendurancepeakMainGreen.png';
+import thumbnailendurancepeakSecond from './jbl endurance peak/thumbnailendurancepeakSecond.png';
+import thumbnailendurancepeakSecondBlue from './jbl endurance peak/thumbnailendurancepeakSecondBlue.png';
+import thumbnailendurancepeakSecondGreen from './jbl endurance peak/thumbnailendurancepeakSecondGreen.png';
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
 import everest300btMain from './jbl everest 300/everest300btMain.png';
 import everest300btMainGreen from './jbl everest 300/everest300btMainGreen.png';
 import everest300btMainPink from './jbl everest 300/everest300btMainPink.png';
 import everest300btSecond from './jbl everest 300/everest300btSecond.png';
 import everest300btSecondGreen from './jbl everest 300/everest300btSecondGreen.png';
 import everest300btSecondPink from './jbl everest 300/everest300btSecondPink.png';
+
+import thumbnaileverest300btMain from './jbl everest 300/thumbnaileverest300btMain.png';
+import thumbnaileverest300btMainGreen from './jbl everest 300/thumbnaileverest300btMainGreen.png';
+import thumbnaileverest300btMainPink from './jbl everest 300/thumbnaileverest300btMainPink.png';
+import thumbnaileverest300btSecond from './jbl everest 300/thumbnaileverest300btSecond.png';
+import thumbnaileverest300btSecondGreen from './jbl everest 300/thumbnaileverest300btSecondGreen.png';
+import thumbnaileverest300btSecondPink from './jbl everest 300/thumbnaileverest300btSecondPink.png';
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
