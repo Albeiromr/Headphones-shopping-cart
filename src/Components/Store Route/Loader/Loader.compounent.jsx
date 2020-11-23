fcd2fe7d0df5d1c0fc5afd18f8fad48cc43e9ca1
Albeiro Marin <xlandru@hotmail.com>
2020-11-23 13:16:16 -0500

    loader animation made

--- src/Components/Store Route/Loader/Loader.compounent.jsx
+++ src/Components/Store Route/Loader/Loader.compounent.jsx
@@ -3,6 +3,7 @@ import Proptypes from "prop-types";
 import "./1000-1920px.style.scss";
 import "./500-1000px.style.scss";
 import "./0-500px.style.scss";
+import animation from '../../../Assets/Img/Logo/animation.png'
 
 
 const Loader = () => {
@@ -10,7 +11,9 @@ const Loader = () => {
     return (
 
         <div className='loader'>
-            
+
+            <img className='loader__img' src={animation} alt="logotype"/>
+
         </div>
     )
 
